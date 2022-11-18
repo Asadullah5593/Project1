6f15a12d8d85c6fe060fffedaa3a96cb8dd7fc20
haseeb-elgrocer <109224480+haseeb-elgrocer@users.noreply.github.com>
2022-11-18 18:00:27 +0500

    Product Module Rewamp.

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const JoiHandler = require("../middlewares/request-validator/request-validator")
 const {createProductRequestSchema} = require("../schemas/validation-schemas/request-validation-schema");
 
 router.post("/create", handler.authHandler, JoiHandler(createProductRequestSchema), ProductController.createProduct);
-router.get("/get-all", ProductController.getAllProducts);
+router.get("/all", ProductController.getAllProducts);
 router.get("/detail/:id", handler.authHandler, ProductController.getProductDetail);
 
 module.exports = router;
